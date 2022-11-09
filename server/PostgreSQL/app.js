dae77bd91cce5055dc172275ca236d9028f84f9f
Ubuntu <ubuntu@ip-172-31-31-9.ec2.internal>
2022-11-09 23:40:41 +0000

    loader.io token & logger

--- server/PostgreSQL/app.js
+++ server/PostgreSQL/app.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 const router = require('./routes.js');
 
 const app = express();
 
 app.use(express.json());
 
+app.use(morgan('dev'));
+
+app.get('/loaderio-de57d1e1c2fc50d8dc33f2dc670e82b2', (req, res) => res.send('loaderio-de57d1e1c2fc50d8dc33f2dc670e82b2'));
+
 app.use(router);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server listening on http://localhost:${process.env.PORT}`);
+  console.log('Server listening');
 });
